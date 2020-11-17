b794d3d0dabd3bad7c56b499c99ef6ee6c687943
MichDist <51273200+MichDist@users.noreply.github.com>
2020-11-17 15:02:00 +0100

    Initial commit

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,29 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    // Hard coded data for testing
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle" },
+                new Command { id = 0, HowTo = "Cur bread", Line = "Use a knife", Platform = "Board" },
+                new Command { id = 0, HowTo = "Make tea", Line = "Teabag", Platform = "Cup" },
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle" };
+        }
+    }
+}
